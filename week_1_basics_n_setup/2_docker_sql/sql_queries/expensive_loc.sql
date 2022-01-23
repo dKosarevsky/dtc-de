1802834b0980305f02952371de064d09de00386e
kosarevskiydp <kosarevskiydp@pik.ru>
2022-01-23 15:12:06 +0300

    add sql query

---
+++ week_1_basics_n_setup/2_docker_sql/sql_queries/expensive_loc.sql
@@ -0,0 +1,14 @@
+with trips as (
+    select concat(zp."Zone", ' / ', zd."Zone") as pickup_dropoff_pair,
+           avg(total_amount)                   as avg_price
+    from yellow_taxi_trips t
+             join zones zp
+                  on t."PULocationID" = zp."LocationID"
+             join zones zd
+                  on t."DOLocationID" = zd."LocationID"
+    group by pickup_dropoff_pair
+)
+select *
+from trips
+order by avg_price desc
+limit 1;
